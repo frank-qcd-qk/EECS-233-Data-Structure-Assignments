2f770f9936a68d19f032b88193c41c415446a8b0
frank-qcd-qk <cxq41@case.edu>
2019-06-13 11:23:44 -0400

    finishes fixing stuff

--- src/week2/problem1/craps.java
+++ src/week2/problem1/craps.java
@@ -12,8 +12,8 @@ import week1.Group1_ex2;
 //The  basic  bet  made  by  the  “shooter” in this game is the pass-line bet. 
 public class craps{
 
-    int comeOutRoll_result=-1;
-
+    private int comeOutRoll_result=-1; //This is used for 3 status!
+    private int comeOutRoll_value = -1; //This is used for storing the last value of dice
 
 
     private final static int WIN = 2;
@@ -25,63 +25,55 @@ public class craps{
 
     //! Construcor...
     public craps(){
-
-    }
-
-
-
-
-    /*
-    This method will help us start the come out roll.
-    It should call the random number generator.
-    */
-    public int comeOutRoll(){
-        int firstRoll = rollDice();
-        switch(firstRoll){
-            case 7:
-            case 11:
-                System.out.println("Value is " + firstRoll + ". Natural win.");
-                return WIN;
-                break;
-            case 2:
-            case 3:
-            case 12:
-                System.out.println("Value is " + firstRoll + ". Craps. Lose.");
-                return LOSE;
-                break;       
-            default:
-                System.out.println("Value is " + firstRoll + ". Roll again.");
-                return NEUTRAL;
-                comeOutRoll_result = firstRoll;
-                break;
+        if (DEBUG){
+            System.out.println("We are in the constructor and nothing should happen!");
         }
     }
 
-
-
+    //! Game runner:
+    public int run(){
+        int comout_result = comeOutRoll();
+        if (comeOutRoll_result == NEUTRAL){
+            int secondround_result = secondRound();
+            return secondround_result;
+        }else{
+            return comout_result;
+        }
+    }
 
 
     //A  come-out  roll  of  2,  3,  or  12  is  called  “craps”  or  “crapping  out,”  and  the  shooter  loses.  
     //A  come-out  roll  of  7  or  11  is  a  “natural,”  and  the  shooter  wins.  
     //The  other possible numbers are the point numbers: 4, 5, 6, 8, 9, and 10. 
     //If the shooter rolls one of  these  numbers  on  the  come-out  roll,  this  establishes  the  “point”—to  win,  
-
-
-
-    private int comeOutRoll_det(){
-        int secondRoll = rollDice();
+    
+    public int comeOutRoll(){
+        this.comeOutRoll_value = rollDice();
+        switch(this.comeOutRoll_value){
+            case 7:
+            case 11:
+                System.out.println("[ComeOutRoll Result:] Value is " + firstRoll + ". Natural win.");
+                return WIN;
+                break;
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("[ComeOutRoll Result:] Value is " + firstRoll + ". Craps. Lose.");
+                return LOSE;
+                break;       
+            default:
+                System.out.println("[ComeOutRoll Result:] Value is " + firstRoll + ". Roll again.");
+                return NEUTRAL;
+                break;
+        }
     }
-
 
     //the  point  number must be rolled again before a seven. 
     //So in the case where a “point” is established 
     //the shooter rolls over and over until either the point is rolled (a win) or a seven is rolled (a loss). 
-    private int secondRound(int previousRoll){
+    private int secondRound(){
 
     }
-
-
-
 
     //! Internal mechanisms:
 
@@ -89,10 +81,10 @@ public class craps{
         int[] dice_2_6_faces = new int[] {1,2,3,4,5,6};
         Group1_ex2 dice_2_6 = new Group1_ex2(6,dice_2_6_faces);
         int[] roll_result = new int[2];
-        
+        int roll_value = -1;
         try{
-        roll_result = dice_2_6.getRollResult();
-        int roll_value = roll_result[0]+roll_result[1];
+            roll_result = dice_2_6.getRollResult();
+            roll_value = roll_result[0]+roll_result[1];
         }catch(ArrayIndexOutOfBoundsException ex){
             System.out.println("[FATAL ERROR!] Roll result array out of bound error!");
         }
@@ -102,11 +94,7 @@ public class craps{
             System.out.println("[Roll Dice Debug] Rolled sum value is: "+roll_result);
         }
 
-        return roll_result;
+        return roll_value;
     }
 
-    private int randomGenerator(int limitation) {
-        Random randomGen = new Random();
-        return randomGen.nextInt(limitation);
-      }
 }
--- src/week2/problem1/main.java
+++ src/week2/problem1/main.java
@@ -1,7 +1,8 @@
 package week2.problem1;
 public class main{
     public static void run(){
-        
+        craps game_single = new craps();
+        game_single.
     }
     //Using your week1.Groupn_Ex2 class for a pair of dice, simulate 100,000 pass-line bets and output how many result in a win, 
     //and how many result in a loss. 
