1298ce79e1c427766d415fd0706101d79d701fd2
Francis O'Brien <fgo3@case.edu>
2019-06-13 17:18:42 -0400

    creating Polynomial constructor

--- src/week2/problem2/Polynomial.java
+++ src/week2/problem2/Polynomial.java
@@ -1,5 +1,14 @@
 package week2.problem2;
 
 public class Polynomial{
-    
+
+    private double coefficient;
+    private double degree; 
+
+    public Polynomial(tuple[] input){
+        for (i=0; i < tuple.length(); i++){
+            this.coefficient = input[i].get_u();
+            this.degree = input[i].get_v();
+        }
+    }
 }
