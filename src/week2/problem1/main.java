053efabb55489c52e2cbaad1a7a159054b062ef7
frank-qcd-qk <cxq41@case.edu>
2019-06-13 11:09:41 -0400

    update runner

--- src/week2/problem1/main.java
+++ src/week2/problem1/main.java
@@ -3,6 +3,9 @@ public class main{
     public static void run(){
         
     }
+    //Using your week1.Groupn_Ex2 class for a pair of dice, simulate 100,000 pass-line bets and output how many result in a win, 
+    //and how many result in a loss. 
+    public static void run_100k(){
 
-    public static void run_100k
+    }
 }
