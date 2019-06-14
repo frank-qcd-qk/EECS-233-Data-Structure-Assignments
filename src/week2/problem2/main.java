47e58467c1be5258a08a0d4ffbe282e103d1b6ff
Francis O'Brien <fgo3@case.edu>
2019-06-14 11:25:27 -0400

    Added in exceptions to the main method

--- src/week2/problem2/main.java
+++ src/week2/problem2/main.java
@@ -4,11 +4,22 @@ import week2.problem2.tuple;
 
 
 public class main{
+
+    public main(){
+    }
+
     public static void main(Strings[] args){
         tuple[] args2tuple;
-        if args%2 != 0
-            throw Error
-
-        polynomial worker = new polynomial()
+        if (args%2 != 0){
+            throw new IllegalArgumentException("All tuples need a coefficient argument and a degree argument.");
+        }
+        try {
+            for (int i = 0; i <args.length; i++)
+            args2tuple = Double.parseDouble(args[i]);
+        } 
+        catch (IllegalArgumentException e){
+            System.out.println("All arguments must be doubles.");
+        }
+        polynomial worker = new polynomial();
     }
 }
