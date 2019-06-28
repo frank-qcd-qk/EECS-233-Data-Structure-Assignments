323f3826f497d0cc8069c1c42ff5beefd813309c
Francis O'Brien <fgo3@case.edu>
2019-06-28 11:54:21 -0400

    added size method

--- src/week3/problem1/MUnboundedArray.java
+++ src/week3/problem1/MUnboundedArray.java
@@ -3,9 +3,13 @@ package week3.problem1;
 public class MUnboundedArray <T extends Object>{
     
     private final static boolean DEBUG = true;
+    private int size;
+    private int subsize;
     private T unArray[][];
 
     public MUnboundedArray(int size, int subsize){
+        this.size = size;
+        this.subsize = subsize;
         this.unArray[size][subsize];
     }
 
@@ -18,6 +22,20 @@ public class MUnboundedArray <T extends Object>{
         this.unArray[index][subIndex] = thing;
     }
 
-
+    public int size(){
+        int fullInd = 0;
+        int fullSubInd = 0;
+        for(int i = 0; i < unArray.length; i++){
+            for(int j = 0; j < unArray[i].length; j++){
+                if (this.unArray[i][j] != null){
+                    fullSubInd++;
+                }
+            }
+            if (this.unArray[i] != null){
+                fullInd++;
+            }
+        }
+        return (fullInd + fullSubInd);
+    }
 }
 
