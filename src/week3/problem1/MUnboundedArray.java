09c7a8651cd3085d137bcba40a0991e31fb9fa6d
Francis O'Brien <fgo3@case.edu>
2019-06-28 11:33:11 -0400

    Constructor, get and set methods

--- src/week3/problem1/MUnboundedArray.java
+++ src/week3/problem1/MUnboundedArray.java
@@ -1,6 +1,23 @@
 package week3.problem1;
 
-public class MUnboundedArray{
+public class MUnboundedArray <T extends Object>{
     
+    private final static boolean DEBUG = true;
+    private T unArray[][];
+
+    public MUnboundedArray(int size, int subsize){
+        this.unArray[size][subsize];
+    }
+
+    public T get(int index, int subIndex){
+        T output = this.unArray[index][subIndex];
+        return output;   
+    }
+
+    public void set(int index, int subIndex, T thing){
+        this.unArray[index][subIndex] = thing;
+    }
+
+
 }
 
