bdfc05a8e0e8cc21d5b068dd07aa04de006a8dee
frank-qcd-qk <cxq41@case.edu>
2019-06-28 16:06:54 -0400

    writed test class

---
+++ src/week3/problem1/testRunner.java
@@ -0,0 +1,11 @@
+package week3.problem1;
+
+public class testRunner{
+    public static void main(String args[]) {
+        MUnboundedArray test = new MUnboundedArray(10);
+        test.grow(9);
+        test.grow(10);
+        test.shrink();
+        System.out.println(test.get(1));
+    }
+}
