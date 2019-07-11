85a7ca85368ed502529403c3ea729fc798ea5741
frank-qcd-qk <cxq41@case.edu>
2019-07-11 16:54:11 -0400

    start the programming

---
+++ src/week4/Scheduler and Generator/src/week4/customDataStructure/frankDS.java
@@ -0,0 +1,14 @@
+package week4.customDataStructure;
+
+import java.util.Queue;
+
+public class frankDS{
+
+    private Object[] prioritizedQueue = new Object[10]; //The orderized priority Queue
+    
+    public frankDS(){
+        for (Object frankQueue : prioritizedQueue) {
+            frankQueue = new Queue[];
+        }
+    }
+}
---
+++ src/week4/Scheduler and Generator/src/week4/customDataStructure/mainRunner.java
@@ -0,0 +1,9 @@
+package week4.customDataStructure;
+
+public class mainRunner{
+    public static void main(String[] args) {
+        frankDS sharedDataStructure = new frankDS();
+
+        
+    }
+}
