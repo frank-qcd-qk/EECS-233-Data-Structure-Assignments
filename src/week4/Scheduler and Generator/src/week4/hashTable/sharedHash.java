8130ae4c8e75150c64a3ae8353dae4363d853dab
Francis O'Brien <fgo3@case.edu>
2019-07-12 16:33:53 -0400

    sharedHash creates hash table, priority determines key. generator method changed to implement the hash table

--- src/week4/Scheduler and Generator/src/week4/hashTable/sharedHash.java
+++ src/week4/Scheduler and Generator/src/week4/hashTable/sharedHash.java
@@ -4,8 +4,21 @@ import java.util.Hashtable;
 
 public class sharedHash{
 
-    public static void main(String[] args){
-        int size = Integer.parseInt(args[0]);
-        Hashtable<Integer, Integer> sharedHash = new Hashtable<Integer, Integer>(size);
+    private int maxSize = 10;
+    Hashtable<Object, Integer> hash = new Hashtable<Object, Integer>(maxSize);
+
+    public int getSize(){
+        return maxSize;
     }
+
+    public int getPriority(Integer key){
+        Integer map = hash.get(key);
+        int priority = map;
+        return priority;
+    }
+
+    public void place(Object insert, Integer key){
+        hash.put(insert, key);
+    }
+
 }
